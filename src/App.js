e9dc52b00d232cff75b9458f822ba59718fae810
ilimmm <whtewn6@gmail.com>
2024-09-22 23:18:05 +0600

    xd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import aviatorImage from './images/aviator.png'; // Импорт изображения
+import hydraLogo from './images/hydralogo.png'; // Импорт логотипа
 
 function App() {
+  const [signal, setSignal] = useState('GET SIGNAL');
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = () => {
+    setLoading(true);
+    // Генерация случайного числа от 1.00 до 9.99
+    setTimeout(() => {
+      const randomCoefficient = (Math.random() * (9.99 - 1.00) + 1.00).toFixed(2);
+      setSignal(`${randomCoefficient}x`);
+      setLoading(false);
+    }, 1000); // Анимация загрузки длится 1 секунду
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-container">
+      <header className="navbar">
+        <img src={hydraLogo} alt="Hydra Logo" className="navbar-logo" />
+        <h1>HYDRA SIGNALS</h1>
       </header>
+      <img src={aviatorImage} alt="Aviator" className="aviator-image" />
+      <div className="container">
+        <button className={`aviator-button ${loading ? 'loading' : ''}`} onClick={handleClick}>
+          {loading ? <div className="spinner"></div> : signal}
+        </button>
+        <a href="https://t.me/yourchannel" className="more-info-button">
+          MORE INFO
+        </a>
+      </div>
     </div>
   );
 }
